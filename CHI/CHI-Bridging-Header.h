66f0e15741ae870cb9fe8225ec90d8277c012d7c
Andy Li <boruil@cs.washington.edu>
2016-05-09 12:14:35 -0700

    adding dependencies, messaging module

---
+++ CHI/CHI-Bridging-Header.h
@@ -0,0 +1,23 @@
+//
+//  CHI-Bridging-Header.h
+//  
+//
+//  Modified by Borui "Andy" Li on 5/9/16.
+//
+//
+
+#ifndef CHI_Bridging_Header_h
+#define CHI_Bridging_Header_h
+
+//
+//  IPMQuickstart-Bridging-Header.h
+//  IPMQuickstart
+//
+//  Created by Kevin Whinnery on 11/29/15.
+//  Copyright © 2015 Twilio. All rights reserved.
+//
+
+#import <TwilioIPMessagingClient/TwilioIPMessagingClient.h>
+
+
+#endif /* CHI_Bridging_Header_h */
